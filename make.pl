94409aacd09acaaed77a31824dd510149f00356e
Justin Davis <jrcd83@gmail.com>
2010-05-09 13:04:26 -0700

    Modify make.pl to clean/delete package dirs.

--- make.pl
+++ make.pl
@@ -2,10 +2,11 @@
 
 use warnings;
 use strict;
-
 use lib q{.};
-use PerlProvides;
-use Template;
+
+use PerlProvides qw(perl_provides);
+use File::Path   qw(remove_tree);
+use Template     qw();
 
 my $tt = Template->new();
 
@@ -14,3 +15,7 @@ open my $pkgbuild_file, '>', 'PKGBUILD' or die "open PKGBUILD:$!";
 $tt->process( 'PKGBUILD.tt',
               { provides => scalar perl_provides( '5.012' ) },
               'PKGBUILD' ) or die "TT process failed: ", $tt->error;
+
+remove_tree( $_ ) for qw/ src pkg /;
+
+system 'makepkg';
