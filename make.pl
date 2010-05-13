97be2effc576906715593da87958795bf1ec1452
Justin Davis <jrcd83@gmail.com>
2010-05-12 17:28:49 -0700

    Add --skiptest option to make.pl.  Add '!strip' to avoid error.

--- make.pl
+++ make.pl
@@ -6,14 +6,20 @@ use lib q{.};
 
 use PerlProvides qw(perl_provides);
 use File::Path   qw(remove_tree);
+use Getopt::Long qw(GetOptions);
 use Template     qw();
+
+my ($SKIP_TEST);
+GetOptions( 'skiptest' => \$SKIP_TEST );
 
 my $tt = Template->new();
 
 open my $pkgbuild_file, '>', 'PKGBUILD' or die "open PKGBUILD:$!";
 
 $tt->process( 'PKGBUILD.tt',
-              { provides => scalar perl_provides( '5.012' ) },
+              { provides => scalar perl_provides( '5.012' ),
+                skiptest => $SKIP_TEST,
+               },
               'PKGBUILD' ) or die "TT process failed: ", $tt->error;
 
 remove_tree( $_ ) for qw/ src pkg /;
